5528fde2623a912ab8d0a17cd4989bea9465c26b
ItsMeStompy <jacob@voisey.co.nz>
2021-10-01 22:36:27 +1300

    Working dark mode
    detects system theme and toggles when pressed

--- js/colorToggle.js
+++ js/colorToggle.js
@@ -1,31 +1,29 @@
-var darkMode = false
-const body = document.body
-let defaultMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+// var darkMode = localStorage.getItem('darkMode');
+var darkMode
+let defaultMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-if(defaultMode) {
-    darkMode = false
-    colorSet()
-} else {
+function enableDarkMode() {
+    document.body.classList.add('dark-mode')
+    // localStorage.setItem('darkmode', true)
     darkMode = true
-    colorSet()
 }
 
-function colorSet() {
-    if (darkMode === true) {
-        document.body.classList.add("dark-mode")
-        document.body.classList.remove("light-mode")
-    } else {
-        document.body.classList.add("light-mode")
-        document.body.classList.remove("dark-mode")
-    }
+function disableDarkMode () {
+    document.body.classList.remove('dark-mode')
+    // localStorage.setItem('darkmode', null)
+    darkMode = null
 }
 
-function colorToggle() {
-    if (darkMode === true) {
-        darkMode = false
-        colorSet()
+if (defaultMode === true) {
+    enableDarkMode();
+}
+
+function darkModeToggle() {
+    // darkMode = localStorage.getItem('darkMode');
+
+    if (darkMode !== true) {
+        enableDarkMode()
     } else {
-        darkMode = true
-        colorSet()
+        disableDarkMode()
     }
 }
